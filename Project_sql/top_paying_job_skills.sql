27752708b5b2db887936175cf47f743033bf6789
kesh99 <keshikawije0817@gmail.com>
2026-04-07 14:41:45 +0530

    top paying skills

--- Project_sql/top_paying_job_skills.sql
+++ Project_sql/top_paying_job_skills.sql
@@ -26,7 +26,8 @@ FROM  top_paying_jobs
 INNER JOIN skills_job_dim ON  top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY 
-    salary_year_avg DESC
+    salary_year_avg DESC;
+
 
 /*
 Based on job postings:
